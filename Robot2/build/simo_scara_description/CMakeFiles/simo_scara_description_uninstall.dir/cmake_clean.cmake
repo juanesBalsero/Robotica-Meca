4f0595f9700ebff33f5aa38ce80b1ebb4a3fe419
Juanes <est.juan.ebalsero@unimilitar.edu.co>
2026-09-01 11:46:43 -0500

    Se eliminó carpeta de Robot2 para crear branch propio

--- Robot2/build/simo_scara_description/CMakeFiles/simo_scara_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/simo_scara_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/simo_scara_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Robot2/src/antro/build/antro/CMakeFiles/antro_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/antro_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/antro_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
